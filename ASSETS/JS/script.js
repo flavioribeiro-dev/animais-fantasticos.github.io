55d568548e527bd39610c7d09d6f479ee2dc02b7
Flavio <flavioxconsult@gmail.com>
2024-10-04 15:06:04 -0300

    concluida animacao de exibicao a partir da tab

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -23,3 +23,57 @@ menuHamburguer.addEventListener('click', () => {
 
 // ================================================
 
+const tabMenu = document.querySelectorAll('.js-tabmenu li');   
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+function activeTab(index) {
+    tabContent.forEach((item) => {
+        item.classList.remove('ativo');
+    })
+    
+    tabContent[index].classList.add('ativo');
+}
+
+tabMenu.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        activeTab(index);
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==============================================
+
+const faqLista = document.querySelectorAll('.faq-lista dt');
+
+faqLista.forEach((faq) => {
+    faq.addEventListener('click', clicou);
+})
+
+function clicou(item) {
+    const faqListaDesc = item.currentTarget.nextElementSibling;
+    console.log( faqListaDesc );
+}
+
+
+
+
+
+
+
+
+
+
+
