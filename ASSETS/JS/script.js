24d99a739cd5873ccc4d873f9e4788128f1e2079
Flavio <flavioxconsult@gmail.com>
2024-10-05 07:13:06 -0300

    concluidas animacoes por Tab e do FAQ, como Accordion List

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,45 +1,50 @@
-const menuHamburguer = document.querySelector('.menu-hamburguer');
-const menuUl = document.querySelector('.menu ul');
+function menuMobile() {
+    const menuHamburguer = document.querySelector('.menu-hamburguer');
+    const menuUl = document.querySelector('.menu ul');
 
-menuHamburguer.addEventListener('click', () => {
-    if(menuHamburguer.getAttribute('aria-hidden') == 'true') {
-        console.log('menu mobile ABERTO');
-        menuHamburguer.removeAttribute('aria-hidden');
-        menuHamburguer.setAttribute('aria-expanded', 'true');
-        menuHamburguer.children[1].style.opacity = 0;
+    menuHamburguer.addEventListener('click', () => {
+        if(menuHamburguer.getAttribute('aria-hidden') == 'true') {
+            console.log('menu mobile ABERTO');
+            menuHamburguer.removeAttribute('aria-hidden');
+            menuHamburguer.setAttribute('aria-expanded', 'true');
+            menuHamburguer.children[1].style.opacity = 0;
 
-        menuHamburguer.children[0].classList.add('rotate_line1');
-        menuHamburguer.children[2].classList.add('rotate_line3');
-    } else {
-        console.log('menu mobile FECHADO');
-        menuHamburguer.removeAttribute('aria-expanded');
-        menuHamburguer.setAttribute('aria-hidden', 'true');
-        menuHamburguer.children[1].style.opacity = 1;     
+            menuHamburguer.children[0].classList.add('rotate_line1');
+            menuHamburguer.children[2].classList.add('rotate_line3');
+        } else {
+            console.log('menu mobile FECHADO');
+            menuHamburguer.removeAttribute('aria-expanded');
+            menuHamburguer.setAttribute('aria-hidden', 'true');
+            menuHamburguer.children[1].style.opacity = 1;     
 
-        menuHamburguer.children[0].classList.remove('rotate_line1');
-        menuHamburguer.children[2].classList.remove('rotate_line3');
-    }
-})
-
+            menuHamburguer.children[0].classList.remove('rotate_line1');
+            menuHamburguer.children[2].classList.remove('rotate_line3');
+        }
+    })
+}
+menuMobile();
 
 // ================================================
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li');   
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function tabActive() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');   
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-function activeTab(index) {
-    tabContent.forEach((item) => {
-        item.classList.remove('ativo');
+    function activeTab(index) {
+        tabContent.forEach((item) => {
+            item.classList.remove('ativo');
+        })
+        
+        tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((item, index) => {
+        item.addEventListener('click', function() {
+            activeTab(index);
+        })
     })
-    
-    tabContent[index].classList.add('ativo');
 }
-
-tabMenu.forEach((item, index) => {
-    item.addEventListener('click', function() {
-        activeTab(index);
-    })
-})
+tabActive();
 
 
 //  ========================================
